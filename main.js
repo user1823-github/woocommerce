ba294b7fb38b8021efbdc240c5333e2877f77986
dev-victus <phatcute585@gmai.com>
2023-04-02 18:11:05 +0700

    version-3

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const btnMenu = document.getElementById('btn-menu')
 const menuPrimary = document.getElementById('primary-navigation')
+const btnAddToCart = document.querySelectorAll('.btn-add-card')
+const loadAddToCart = document.querySelectorAll('.follow-the-leader')
 
 btnMenu.addEventListener('click', () => {
     menuPrimary.classList.toggle('active')
 })
+
+btnAddToCart.forEach(item => {
+    item.addEventListener('click', () => {
+        loadAddToCart.
+    })
+})
+
