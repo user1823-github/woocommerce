0e381c34953c381f4e74056da9efe6581b5b5de8
dev-victus <phatcute585@gmai.com>
2023-04-04 04:46:51 +0700

    version-4

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuPrimary = document.getElementById('primary-navigation')
 const btnAddToCart = document.querySelectorAll('.btn-add-card')
 const loadAddToCart = document.querySelectorAll('.follow-the-leader')
 
-console.log(btnAddToCart);
+// console.log(btnAddToCart);
 // btnMenu.addEventListener('click', () => {
 //     menuPrimary.classList.toggle('active')
 // })
@@ -24,31 +24,62 @@ btnAddToCart.forEach(btn => {
         let productImg = product.querySelector("img").src;
         // console.log(productName, productPrice, productImg);
         addCart(productImg, productName, productPrice)
+        closeButton()
     })
 })
 
 function addCart(productImg, productName, productPrice) {
-    let cartBox = document.querySelector('.item')
-    let cartContent = document.createElement('a')
-    cartContent.classList.add('cartContent')
+    let cartBox = document.querySelector('.cart-item')
+    let cartContent = document.createElement('div')
+    cartContent.classList.add('item')
+
     let cartItem = `
+    <a href="" class="cart-item-link">
         <div class="cart-close">
             <div class="svg-close-cart">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg class="close-btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </div>
             <span class="product-cart-name">
-                ${productName} 
+                ${productName}asdadadadsdsadasdsadadasdsssssssssssssssssssssss
             </span>					                                              
         </div>
         <div class="cart-img">
-            <img src="${productImg.slice(33)}" alt="">
+            <img src="${'.' + productImg.slice(33)}" alt="">
         </div>
-        <span class="quantity">${productPrice.slice(1)}</span>
+    </a>
+    <span class="quantity">1 x £${productPrice.slice(1)}</span>
     `
-    // let text = productPrice
     cartContent.innerHTML = cartItem
-    // console.log(productPrice.slice(1), productImg.slice(33));
     cartBox.append(cartContent)
+
+    totalCartItems()
+}
+
+function closeButton() {
+    const closeButton = document.querySelectorAll('.close-btn')
+
+    closeButton.forEach(btn => { 
+      btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        let cartContent = btn.closest('.item')
+        cartContent.remove()
+        totalCartItems()
+      })
+    })
+}
+
+function totalCartItems() {
+    let priceItem = document.querySelectorAll('.quantity')
+    let totalItems = document.querySelector('.cart-total')
+    // Total
+    let sum = 0;
+    priceItem.forEach(item => {
+        sum += Number(item.innerText.slice(5))
+    })
+    
+    totalItems.innerHTML = `
+    Subtotal: £${sum}
+    `
 }
